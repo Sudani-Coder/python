0e9e8ae738d6838a8b0eb780310a46ef3389c369
Sudani Coder <sudani.coder@gmail.com>
2019-11-03 14:29:23 +0200

    add array data structure python script

---
+++ Data Structure/Array/index.py
@@ -0,0 +1,8 @@
+fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi", "Lemon", "Pineapple"]
+numbers = [85, 25, 5, 15, 1, 30, 20, 90, 70, 45, 60, 40, 55, 100, 50, 95, 65, 10, 80, 75, 35]
+
+for fruit in fruits:
+    print(fruit, end = "\n\n")
+
+for num in numbers:
+    print(num)
