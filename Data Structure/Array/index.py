a1f474da2c6bafa9b083362ab6ae92a786aeeae4
Sudani Coder <sudani.coder@gmail.com>
2019-11-03 20:04:26 +0200

    create TheMax and TheMin Functions

--- Data Structure/Array/index.py
+++ Data Structure/Array/index.py
@@ -6,3 +6,20 @@ for fruit in fruits:
 
 for num in numbers:
     print(num)
+
+print()
+def TheMax(MyList):
+    maximum = MyList[0]
+    for num in MyList:
+        if num > maximum:
+            maximum = num
+    return maximum
+
+def TheMin(MyList):
+    minimum = MyList[0]
+    for num in MyList:
+        if num < minimum:
+            minimum = num
+    return minimum
+
+print("Maximum Number = {}".format(TheMax(numbers)), "Minimum Number = {}".format(TheMin(numbers)), sep = "\n\n", end = "\n\n")
