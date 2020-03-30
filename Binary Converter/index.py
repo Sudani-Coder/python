caf106927ccd9f4d1e1e3aa80b33e21be1ac75b5
Sudani Coder <sudani.coder@gmail.com>
2020-03-30 08:54:17 +0200

    add try-excpet code to binary converter

--- Binary Converter/index.py
+++ Binary Converter/index.py
@@ -1,8 +1,13 @@
 ## Project: 1
 # Convert a Number into Binary, Octal and Hexadecimal
 
-dec_num = int(input("Enter the number: "))
+while True:
+    try:
+        dec_num = int(input("\n Enter the number: "))
+        
+        print("\n Binary value is {} ".format(bin(dec_num)))
+        print("\n Octal value is {} ".format(oct(dec_num)))
+        print("\n Hexadecimal value is {} ".format(hex(dec_num)))
 
-print("Binary value is {} ".format(bin(dec_num)))
-print("Octal value is {} ".format(oct(dec_num)))
-print("Hexadecimal value is {} ".format(hex(dec_num)))
+    except ValueError as error:
+        print("\n {}\n value error please enter a number.".format(error))
